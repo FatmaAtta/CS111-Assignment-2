01d673c919cd61107974eb3862e13a056d5d7329
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 12:35:01 +0200

    Update Q4.py

--- Problem4-Solution/Q4.py
+++ Problem4-Solution/Q4.py
@@ -1,4 +1,7 @@
-
+#Q4. You need to write an algorithm for finding the factorial of a non-negative integer n.
+#Recall that the factorial of n is defined as the product: 1×2 × 3 × …..× (n-1) × n The factorial of 0 is defined to be 1.
+#Draw a flowchart diagram for your algorithm. Test the execution of the flowchart step-by-step on n = 0, 2, 6, 10
+# Implement the algorithm as a Python script. Test the execution of the flowchart step-by-step on n = 0, 2, 6, 10
 def factorial(n):
     if n==0 or n==1:
         fact=1
@@ -7,5 +10,3 @@ def factorial(n):
     return fact
 n=int(input("Enter a number \n"))
 print(n,"! = ",factorial(n))
-
-
