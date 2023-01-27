8a0c33279a811d7c60e41f2143f082c107369287
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 15:06:19 +0200

    Update Q17.py

--- Problem17-Solution/Q17.py
+++ Problem17-Solution/Q17.py
@@ -1,3 +1,8 @@
+#Q17. We want to write a program to convert a positive decimal number to the equivalent binary number in one byte. We already do this algorithm by hand, but we want to formulate it in the form of pseudocode so we can program it in Python. Your algorithm should:
+#1) Take the input from the user.
+#2) Check if it is in the range that can be represented in one byte. If not, show an error message.
+#3) Convert it to binary and display the result.
+
 num = int(input("Please enter a positive number\n"))
 if(num<0):
     num=int(input("Please enter a positive number\n"))
