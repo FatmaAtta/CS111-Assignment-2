246718fefdc1baca69889b3905cdcba60283011c
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 12:40:01 +0200

    Update Q7.py

--- Problem7-Solution/Q7.py
+++ Problem7-Solution/Q7.py
@@ -1,3 +1,6 @@
+#Q7. Suppose A is an array of 100 elements. Write a for-loop that stores the elements of A in reverse order in a new array X.
+#That is, A[0] must go to X[99], A[1] must go to X[98] and so on.
+
 a=[]
 x=[]
 a.extend(range(1,101))
