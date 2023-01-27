b537432315fecafae1053d9718cc6ecb3028729a
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 15:08:39 +0200

    Update Q27.py

--- Problem27-Solution/Q27.py
+++ Problem27-Solution/Q27.py
@@ -1,3 +1,5 @@
+#Q27. Exercise 3: Write a Python program to calculate the sum of the digits in an integer
+
 num = input("enter an integer\n")
 length=len(num)
 number=[int(x) for x in num]
@@ -6,5 +8,3 @@ sum=0
 for i in range(0,length):
     sum+=int(number[i])
 print(sum)
-
-
