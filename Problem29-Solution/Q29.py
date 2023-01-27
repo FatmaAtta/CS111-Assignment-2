3b6f3b9b105eabd24c304d6abf1b7f7d05d95a85
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 15:10:42 +0200

    Update Q29.py

--- Problem29-Solution/Q29.py
+++ Problem29-Solution/Q29.py
@@ -1,3 +1,7 @@
+#Q29. Exercise 5: Write a python program that takes two integer values (start and end). This program will
+#find all numbers (between start and end) which are divisible by 9 but are not divisible by 4.
+#The numbers obtained should be printed in a comma-separated sequence on a single line
+
 starting=int(input("Enter start\n"))
 ending=int(input("Enter end\n"))
 for i in range(starting,ending):
