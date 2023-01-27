62d46910795c28bb3e9eaa0e65c7b5ea85f12bca
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 13:18:36 +0200

    Update Q13.py

--- Problem13-Solution/Q13.py
+++ Problem13-Solution/Q13.py
@@ -1,3 +1,5 @@
+#Q13. Design an algorithm for finding all the factors of a positive integer.
+#For example, in the case of the integer 12, your algorithm should report the values 1, 2, 3, 4, 6, and 12. 
 num = int(input("Enter a positive number\n"))
  #lets user re-enter a number if number was negative
 while num<0:
