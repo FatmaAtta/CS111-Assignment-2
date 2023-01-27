48e24feeb5ac2c10c1179bd84ac04eebcbc5b661
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 15:09:38 +0200

    Update Q28.py

--- Problem28-Solution/Q28.py
+++ Problem28-Solution/Q28.py
@@ -1,3 +1,8 @@
+#Q28. Exercise 4: update exercise 3 to check whether a given number is an Armstrong number or not.
+#A positive integer is called an Armstrong number if the sum of the cubes of individual digits of the
+#number is equal to that number itself. For example, the sum of cubes of individual digits of the number
+#153 is 1* 1 * 1 + 5 * 5 * 5 + 3 * 3 * 3 = 153. Hence, number 153 is an Armstrong number.
+
 num = input("enter an integer\n")
 length=len(num)
 number=[int(x)**3 for x in num]
@@ -9,6 +14,3 @@ if str(sum)==num:
     print(num, " is an Armstrong number\n")
 else:
     print(num, " is not an Armstrong number\n")
-
-
-
