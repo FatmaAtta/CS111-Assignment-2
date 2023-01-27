49f47bddf8f2399bcb3cca767ec641d2d8636fa7
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 12:38:14 +0200

    Update Q6.py

--- Problem6-Solution/Q6.py
+++ Problem6-Solution/Q6.py
@@ -1,3 +1,6 @@
+#Write a while-loop to swap every successive pair of elements in an array A of 50 elements,
+#i.e., A[0] and A[1] should be swapped, A[2] and A[3] should be swapped and so on.
+
 elements = [i for i in range(1,51)]
 print(elements)
 print("\n")
