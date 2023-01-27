ed1055975fc1e8171babdd234285c3756a61c934
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 15:28:50 +0200

    Update Q40.py

--- Problem40-Solution/Q40.py
+++ Problem40-Solution/Q40.py
@@ -1,3 +1,6 @@
+#Q40. Exercise 16: Write a python function that takes an integer number (size) then it generates Fibonacci sequence with the given size. (Hint: The Fibonacci sequence is a sequence of numbers where the next number in the sequence is the sum of the previous two numbers in the sequence. The sequence
+looks like this: 1, 1, 2, 3, 5, 8, 13, …)
+
 def fib(size):
     a,b=0,1
     print('1 ',end='')
