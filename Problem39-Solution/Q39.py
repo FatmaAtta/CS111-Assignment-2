7de2b4e6cf52ccb0330334e16e60c5f0b1a23d6a
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 15:28:04 +0200

    Update Q39.py

--- Problem39-Solution/Q39.py
+++ Problem39-Solution/Q39.py
@@ -1,3 +1,6 @@
+#Q39. Exercise 15: Write an algorithm and draw a flowchart to print the SUM of numbers from LOW to
+#HIGH. Test with LOW=3 and HIGH=9.(2) Write a Python program to implement this algorithm.
+
 sum = 0
 low=int(input("Enter low\n"))
 high=int(input("Enter high\n"))
