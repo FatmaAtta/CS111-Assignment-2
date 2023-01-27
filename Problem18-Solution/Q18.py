445d845eea92f6f4aceca00618b7a8a73f2b7ae8
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 15:07:24 +0200

    Update Q18.py

--- Problem18-Solution/Q18.py
+++ Problem18-Solution/Q18.py
@@ -1,3 +1,7 @@
+#Q18. The German mathematician Leibniz (1646–1716) discovered the rather remarkable fact that the
+#mathematical constant () Pi ط can be computed using the following mathematical relationship:
+#The formula to the right of the equal sign represents an infinite series; each fraction represents a term in that series. If you start with 1, subtract one-third, add one-fifth, and so on, for each of the odd integers, you get a number that gets closer and closer to the value of /4 as you go along. Then we can get .
+
 i,m,summation = 1,0,0.0
 n = int(input("Enter the number of terms \n"))
 for x in range(0,n):
@@ -5,5 +9,3 @@ for x in range(0,n):
     i=i+2
     m=m+1
 print("Pi = ",4*summation)
-
-
