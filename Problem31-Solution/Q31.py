6073881e0ff7c4c311127582f1a85a6b6e2b4ebc
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 15:13:52 +0200

    Update Q31.py

--- Problem31-Solution/Q31.py
+++ Problem31-Solution/Q31.py
@@ -1,3 +1,5 @@
+#Q31. Exercise 7: Write a Python program to take from the user three integer numbers (x,y, z) and your program should search about an operations 
+#(e.g., +, -,*,%, etc) that can be executed on both x and y such that the result should be z, then you program would print x operations y = z
 def operation(x,y,z):
     if x+y==z:
         print(x,' + ',y,' = ',z)
@@ -17,7 +19,3 @@ x=int(input("Enter x\n"))
 y=int(input("Enter y\n"))
 z=int(input("Enter z\n"))
 operation(x,y,z)
-
-
-
-
