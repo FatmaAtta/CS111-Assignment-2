0d820f8a9cde67809c53bd8eb24a8b54134c49a9
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 15:24:37 +0200

    Update Q35.py

--- Problem35-Solution/Q35.py
+++ Problem35-Solution/Q35.py
@@ -1,3 +1,5 @@
+#Q35. Exercise 11: Write a Python program that will take a non-empty string and an int n, return a new string where the char at index n has been removed.
+#The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive)
 string=input("Enter a string\n")
 n=int(input("Enter the index\n"))
 if n<len(string) and n>=0:
