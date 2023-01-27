72e525d5fb12dbd9b9b7c08db2e788b026a725fa
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 12:36:19 +0200

    Update Q5.py

--- Problem5-Solution/Q5.py
+++ Problem5-Solution/Q5.py
@@ -1,3 +1,4 @@
+#Q5. Write an algorithm to take a string from the user then reverse it (e.g. if it contains abbxc, then the output will be cxbba).
 #reverses the string
 string=[]
 string=input("Enter a string \n")
