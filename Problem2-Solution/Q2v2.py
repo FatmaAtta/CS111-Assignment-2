2c0fb5188feb6de74298a9c8d7e37d4b63a9e258
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 12:33:18 +0200

    Update Q2v2.py

--- Problem2-Solution/Q2v2.py
+++ Problem2-Solution/Q2v2.py
@@ -1,3 +1,10 @@
+#Q2. Assume that you are given time-of-the-day in the format hours-minutes-seconds, 
+#using 24 hours format. Devise an algorithm to subtract one time from another. 
+#The specification is: Input: integers h1, m1, s1 – representing a time,
+#e.g. 13, 30, 12 h2, m2, s2 – represents another time that is prior to the first time,
+#e.g., 11,40, 56. Output: integers h, m, s – difference between the two times in hours-minutes-seconds
+#format. For the examples above, this will be 1, 49. 16
+
 time1=input("Enter time 1\n").split(',')
 time2=input("Enter time 2\n").split(',')
 
