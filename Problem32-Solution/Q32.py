ca810b13f6326e92dbcc821701fd78a847f2c288
FatmaAtta <87873165+FatmaAtta@users.noreply.github.com>
2023-01-27 15:22:36 +0200

    Update Q32.py

--- Problem32-Solution/Q32.py
+++ Problem32-Solution/Q32.py
@@ -1,3 +1,8 @@
+#Q32. Exercise 8: Given a word of five characters in lower case, write a flowchart and pseudo code to
+#convert this word to upper case. Example: “apple” should be “APPLE.
+#Hint: To get the ascii code of character 'a' for example, use ascii('a') and to convert ascii code 97 for
+#example back to character, use character(97)
+
 upCase=[0,0,0,0,0]
 word=input("Enter a word of 5 letters \n")
 while len(word)>5:
